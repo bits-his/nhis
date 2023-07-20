4a5ba07fba317c550eceb30d0ebf9aa8bbacb2af
ahmad kabir <atomakabir001@gmail.com>
2023-07-20 15:02:15 +0100

    save

--- src/routes/AppNavigation.jsx
+++ src/routes/AppNavigation.jsx
@@ -1,21 +1,35 @@
 import { useRoutes } from 'react-router-dom'
 import AppIndex from './AppIndex'
 import VerificationNew from '../components/VerificationNew'
-import VerificationList from '../components/VerificationList'
+// import VerificationList from '../components/VerificationList'
 import ViewPrincipal from '../components/ViewPrincipal'
 import ExcelImporter from '../components/ExcelImporter'
 import Project from '../components/Project'
+import Signin from '../components/Signin'
+import Dashboard from '../components/Dashboard'
+import VerificationList from '../components/VerificationList'
 
 function AppNavigation() {
   let element = useRoutes([
     {
       path: "/",
+      element: <Signin />,
+      children: [{ index: true }],
+    },
+    {
       element: <AppIndex />,
       children: [
-        { index: true, element: <VerificationList /> },
         {
-          path: "/new-verification",
-          element: <VerificationNew />,
+            path: "/dashboard",
+            element: <Dashboard />,
+        },
+        {
+            path: "/verification-list",
+            element: <VerificationList />,
+        },
+        {
+            path: "/new-verification",
+            element: <VerificationNew />,
         },
         {
           path: "/view-principal",
@@ -29,9 +43,9 @@ function AppNavigation() {
           path: "/create-new-list",
           element: <Project />,
         },
-      ],
-    },
-  ]);
+      ]
+    }
+  ])
   return element;
 }
 export default AppNavigation;
